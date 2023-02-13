26a8e5fdbbe8a3e2e97ebc43fc9f16c2a6856bda
Robi <robi.maliqi@hotmail.co.uk>
2023-02-13 22:12:51 +0000

    added post tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "/posts/new" do
+    it "succeeds" do
+      get new_post_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "posts/create" do
+    context "valid params" do
+      it "successfully creates a post" do
+        expect do
+          post posts_path, params: {
+            post: {
+              title: "example title",
+              body: "example body"
+            }
+          }
+        end.to change { Post.count }.from(0).to(1)
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    context "invalid params" do
+      it "fails at creating the post" do
+        expect do
+          post posts_path, params: {
+            post: {
+              title: "",
+              body: ""
+            }
+          }
+        end.not_to change { Post.count }
+        expect(Post.count).to eq(0)
+        expect(response).to have_http_status(:success)
+      end
+    end
   end
 end
